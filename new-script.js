e9818290d8b66694da554ee6c748c46fa597973d
joandercim <joacim@josiasmusik.se>
2023-11-07 15:51:29 +0100

    mostly styling

--- new-script.js
+++ new-script.js
@@ -6,6 +6,7 @@ class App {
     this.fetchedCities = [];
     this.matches = [];
     this.responseContainer = document.getElementById('response');
+    // this.success = true;
   }
 
   async getCities() {
@@ -15,11 +16,13 @@ class App {
   }
 
   async getWeatherByCity() {
+    this.showSpinner();
     const cities = await this.getCities();
 
     for (const city of cities) {
       await this.fetchCurrentWeather(city);
     }
+    this.hideSpinner();
   }
 
   async fetchCurrentWeather(inputCity) {
@@ -35,12 +38,21 @@ class App {
       }
 
       const data = await res.json();
-      // Timeseries[0] bestämmer att visa vädret just nu
+      // Create new city with data from local json and SMHI
       const storedCity = new newCity(city, lat, lng, data);
+
+      const date = new Date().toLocaleString();
+      document.querySelector(
+        '.last-update'
+      ).textContent = `Weather information fetched ${date}`;
 
       this.fetchedCities.push(storedCity);
     } catch (error) {
+      this.success = false;
       console.log(`Ett fel uppstod vid hämtning av ${city}: ${error}`);
+      document.querySelector('.last-update').textContent =
+        'No data collected, see console for more infomation.';
+      document.querySelector('.last-update').style.color = 'red';
     }
   }
 
@@ -58,32 +70,44 @@ class App {
   }
 
   interpretUserInput(userInput) {
+    let backgroundElement = document.querySelector('.overlay');
     let weatherCodes = [];
+
     switch (userInput) {
-      case 'sunny':
-        weatherCodes = [1, 2];
-        break;
-      case 'halfclear':
-        weatherCodes = [3, 4];
+      case 'clear-skies':
+        weatherCodes = [1, 2, 3];
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1550133730-695473e544be?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         break;
       case 'cloudy':
         weatherCodes = [5, 6];
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1485249245068-d8dc50b77cc7?q=80&w=2662&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         break;
       case 'foggy':
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1541480110211-586977e40589?q=80&w=2693&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         weatherCodes = [7];
         break;
       case 'raining':
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1519692933481-e162a57d6721?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         weatherCodes = [8, 9, 10, 18, 19, 20];
         break;
       case 'thunder':
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1431440869543-efaf3388c585?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         weatherCodes = [21, 11];
         break;
       case 'snowing':
         weatherCodes = [25, 26, 27, 13, 14, 15, 16, 17];
+        backgroundElement.style.backgroundImage =
+          'url(https://images.unsplash.com/photo-1577457943926-11193adc0563?q=80&w=2702&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
         break;
     }
     this.findMatchingWeather(weatherCodes);
-    this.displayMatches(userInput);
+    const userInputFormatted = userInput.replace('-', ' ');
+    this.displayMatches(userInputFormatted);
   }
 
   findMatchingWeather(weatherCodes) {
@@ -115,7 +139,14 @@ class App {
       return;
     }
 
-    h2.textContent = `Yes! Today there is supposed to be ${userInput} in ${this.matches.length} cities.`;
+    // if (this.success === false) {
+    //   h2.textContent = `Something went wrong wile fetching the data. Please come back later`;
+    //   this.responseContainer.appendChild(h2);
+    //   this.responseContainer.append(responseCards);
+    //   console.log('WERJKFLDSJKLFJDKSLJFLKSD');
+    // }
+
+    h2.textContent = `Yes! There currently ${userInput} in ${this.matches.length} cities.`;
     this.responseContainer.appendChild(h2);
     responseCards.className = 'response-cards';
     this.responseContainer.append(responseCards);
